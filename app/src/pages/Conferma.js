463184cdc6f3d276465abfade0c34ce90ba87f8b
amaddaloni12 <a.maddaloni12@studenti.unisa.it>
2022-05-30 19:16:47 +0200

    Aggiunte pagine Inserimento Portata e Conferma

--- app/src/pages/Conferma.js
+++ app/src/pages/Conferma.js
@@ -1,18 +1,41 @@
+import { Button, Grid, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import useTitleContext from "../components/PageTitleContext";
-
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const Conferma = () => {
-    const { changeTitle } = useTitleContext();
-    useEffect(() => {
-        changeTitle("Pagina di conferma");
-    }, []);
+  const { changeTitle } = useTitleContext();
+  useEffect(() => {
+    changeTitle("Pagina di conferma");
+  }, []);
 
   return (
-      <div>
-         ciao 
-      </div>
-
+    <div>
+      <Grid container justifyContent="center" alignItems="center" margin="auto">
+        <Card sx={{ width: 400 }}>
+          <div
+            style={{
+              backgroundColor: "#fbe9e7",
+            }}
+          >
+            <CardContent>
+              <CheckCircleIcon color="secondary" sx={{ fontSize: 180 }} />
+              <Typography variant="h5" component="div">
+                L’inserimento è andato a buon fine!
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" size="small" color="secondary">
+                Ok
+              </Button>
+            </CardActions>
+          </div>
+        </Card>
+      </Grid>
+    </div>
   );
 };
 
--- app/src/pages/FormInsPortata.js
+++ app/src/pages/FormInsPortata.js
@@ -1,28 +1,147 @@
-import { Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Grid,
+  List,
+  ListItem,
+  MenuItem,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import useTitleContext from "../components/PageTitleContext";
 
-const FormInsPortata = () => {
-    const { changeTitle } = useTitleContext();
-    useEffect(() => {
-        changeTitle('Inserimento della portata')
-  }, [])
+const allergeni = [
+  {
+    value: "1",
+    label: "lattosio",
+  },
+  {
+    value: "2",
+    label: "glutine",
+  },
+  {
+    value: "3",
+    label: "crostacei",
+  },
+  {
+    value: "4",
+    label: "frutta a guscio",
+  },
+  {
+    value: "5",
+    label: "nichel",
+  },
+  {
+    value: "6",
+    label: "proteina LTP",
+  },
+];
 
-  return(
-      <div>
-          <Grid container maxWidth={'md'} justifyContent="center" alignItems="center">
-                <Grid item xs={12} md={12} lg={6}>
-                    <Typography variant="h4" component="h1">
-                        Aggiungi un nuovo piatto
-                    </Typography>
-                
-                    <Typography>
-                        Aggiungi un nuovo piatto compilando i campi del seguente form
-                    </Typography>
+const FormInsPortata = () => {
+  let navigate = useNavigate();
+
+  const { changeTitle } = useTitleContext();
+  useEffect(() => {
+    changeTitle("Segnalazione di un locale");
+  }, []);
+
+  return (
+    <div sx={{ bgcolor: "#FAF4F4" }}>
+      <Grid container>
+        <Grid item xs={12} md={2} lg={3} />
+        <Grid item xs={12} md={8} lg={6}>
+          <Paper>
+            <Typography
+              sx={{ mr: 2, ml: 2, mb: 2 }}
+              variant="h4"
+              component="h1"
+            >
+              {" "}
+              Aggiungi un nuovo piatto
+            </Typography>
+            <Typography sx={{ mr: 2, ml: 2, mb: 2 }}>
+              {" "}
+              Crea un nuovo piatto aggiungendo le infomazioni necessarie, non
+              dimenticare di aggiungere eventuali allergeni{" "}
+            </Typography>
+            <List>
+              <ListItem>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  {" "}
+                  Informazioni sul piatto{" "}
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <TextField fullWidth id="NomePiatto" label="Nome del piatto" />
+              </ListItem>
+              <ListItem>
+                <TextField
+                  fullWidth
+                  id="Descrizione"
+                  label="Breve descrizione"
+                />
+              </ListItem>
+              <ListItem>
+                <TextField fullWidth id="Foto" label="Aggiungi una foto" />
+              </ListItem>
+              <ListItem>
+                <TextField
+                  fullWidth
+                  select
+                  id="Allergene"
+                  label="Scegli l'allergene non presente"
+                >
+                  {allergeni.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </ListItem>
+
+              <ListItem>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} md={6}>
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="secondary"
+                      style={{ color: "white" }}
+                      onClick={() => {
+                        navigate("/conferma");
+                      }}
+                    >
+                      Conferma
+                    </Button>
+                  </Grid>
+
+                  <Grid item xs={12} md={6}>
+                    <Button
+                      fullWidth
+                      variant="outlined"
+                      color="secondary"
+                      onClick={() => {
+                        navigate(-1);
+                      }}
+                    >
+                      Annulla
+                    </Button>
+                  </Grid>
                 </Grid>
-            </Grid>
-      </div>
-  )
+              </ListItem>
+            </List>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={2} lg={3} />
+      </Grid>
+    </div>
+  );
 };
 
 export default FormInsPortata;
